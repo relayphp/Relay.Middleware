eb190a140965ac6aa9a6101bcb9351b6882bec21
Paul M. Jones <pmjones88@gmail.com>
2015-11-10 16:16:09 -0600

    minor style changes

--- src/JsonDecoder.php
+++ src/JsonDecoder.php
@@ -9,29 +9,27 @@ class JsonDecoder
     protected $assoc;
     protected $maxDepth;
     protected $options;
-    
+
     public function __construct($assoc = false, $maxDepth = 256, $options = 0)
     {
         $this->assoc = $assoc;
         $this->maxDepth = $maxDepth;
         $this->options = $options;
     }
-    
+
     public function __invoke(Request $request, Response $response, callable $next)
     {
-        $parts  = explode(';', $request->getHeaderLine('Content-Type'));
-        $type   = trim(array_shift($parts));
-        $method = $request->getMethod();
+        $parts = explode(';', $request->getHeaderLine('Content-Type'));
+        $type = strtolower(trim(array_shift($parts)));
 
-        if ('GET' != $method
-            && ! empty($type)
-            && 'application/json' == strtolower($type)
-        ) {
-            $body    = (string) $request->getBody();
-            $request = $request->withParsedBody(json_decode($body,
-                                                            $this->assoc,
-                                                            $this->maxDepth,
-                                                            $this->options));
+        if ($request->getMethod() != 'GET' && $type == 'application/json') {
+            $body = (string) $request->getBody();
+            $request = $request->withParsedBody(json_decode(
+                $body,
+                $this->assoc,
+                $this->maxDepth,
+                $this->options
+            ));
         }
 
         return $next($request, $response);
